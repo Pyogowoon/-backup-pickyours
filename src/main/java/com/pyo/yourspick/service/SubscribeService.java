92cb7ccceb9b5f67f5b9c7b1cfd5f36858f9afc0
pyo <vytjdgus1234@naver.com>
2023-02-21 19:00:28 +0900

    aws test

--- src/main/java/com/pyo/yourspick/service/SubscribeService.java
+++ src/main/java/com/pyo/yourspick/service/SubscribeService.java
@@ -47,7 +47,7 @@ public class SubscribeService {
         sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
         sb.append("if ((SELECT 1 FROM Subscribe WHERE fromUserId = ? AND toUserId = u.id), 1,0) subscribeState, ");
         sb.append("if ((?=u.id), 1, 0) equalUserState ");
-        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("FROM User u INNER JOIN Subscribe s ");
         sb.append("ON u.id = s.toUserId ");
         sb.append("WHERE s.fromUserId = ?");
 
