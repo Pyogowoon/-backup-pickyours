0ff9e5f6c06339f6613343ac417ac03d0cf9cc33
pyo <vytjdgus1234@naver.com>
2023-02-22 15:38:31 +0900

    git

--- src/main/java/com/pyo/yourspick/Pickyours_devApplication.java
+++ src/main/java/com/pyo/yourspick/Pickyours_devApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YourspickApplication {
+public class Pickyours_devApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YourspickApplication.class, args);
+		SpringApplication.run(Pickyours_devApplication.class, args);
 	}
 
 }
